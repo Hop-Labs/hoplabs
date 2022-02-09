0f397a1583a9baeb82d1dd36a8080e6347115d30
Sebastian Cabrejos <14218992+Boccca2014@users.noreply.github.com>
2022-02-09 08:21:42 -0500

    trigger vercel hook

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
     <div className="h-screen w-screen p-10 dark:bg-gray-900 bg-indigo-200">
       <MainNav />
       <main className="items-center py-10">
-        <h1 className="text-3xl font-bold"> Hello world! </h1>
+        <h1 className="text-3xl font-bold"> Hop Labs </h1>
       </main>
       <footer className=""></footer>
     </div>
